53a37f005fdc55434f5d12ffa9adf2d2aae67f69
Tural <turaltalybov4@gmail.com>
2026-04-05 16:13:02 +0700

    auto deploy 2026-04-05 16:13:01

---
+++ deploy_autobot2.ps1
@@ -0,0 +1,26 @@
+param(
+    [string]$ServerIp = "146.103.42.126",
+    [string]$ManagerIds = "8673882143",
+    [string]$Profile = "turbo_service",
+    [string]$RepoUrl = "https://github.com/turaltalybov4-afk/avto_bot1.git"
+)
+
+$ErrorActionPreference = "Stop"
+Set-Location -Path $PSScriptRoot
+
+$token = Read-Host "Paste TOKEN for autobot2"
+if ([string]::IsNullOrWhiteSpace($token)) {
+    throw "Token is required"
+}
+
+powershell -ExecutionPolicy Bypass -File ".\one_click_deploy.ps1" `
+    -ServerIp $ServerIp `
+    -ServiceName "autobot2" `
+    -BotUser "botuser2" `
+    -RepoDir "/opt/auto_bot2" `
+    -DataDir "/var/lib/auto_bot2" `
+    -EnvFile "/etc/auto_bot2.env" `
+    -BotToken $token `
+    -ManagerIds $ManagerIds `
+    -Profile $Profile `
+    -RepoUrl $RepoUrl
